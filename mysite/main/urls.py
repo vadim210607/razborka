83fd22496bb23fa847d077b91a72b5e0b1dc53e0
vadim210607 <vadim210607@gmail.com>
2023-03-06 00:28:17 +0200

    add filter category

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 
-
 from .views import *
 
 from django.conf import settings
@@ -10,10 +9,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', index, name='home'),
     path('about', about, name='about'),
-    path('renault-parts/', parts, name="catalog"),
-    path('renault-parts/q/<int:item_id>/', item, name="item"),
-    path('renault-parts/<int:category_id>/', show_category, name="category"),
-    path("postuser/", postuser),
+    # path('renault-parts/', parts, name="catalog"),
+    path('renault-parts/', SearchResult.as_view(), name="catalog"),
+    # path('renault-parts/q/<int:item_id>/', item, name="item"),
+    # path('renault-parts/<int:category_id>/', show_category, name="category"),
+    # path("postuser/", postuser),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import *
+from django.db.models import Q
+from django.views.generic import TemplateView, ListView
 
 
 def index(request):
-    return render(request, "main/index.html", {"title": "Головна сторінка",})
+    auto = Auto.objects.all()
+    category = Category.objects.all()
+    data = {
+        "title": "Головна сторінка",
+        "auto": auto,
+        "category": category,
+    }
+    return render(request, "main/index.html", context=data)
 
 def about(request):
     return render(request, "main/about.html")
@@ -13,47 +22,66 @@ def about(request):
 def item(request, item_id):
     return HttpResponse(f'Відображення статті з ID = {item_id}')
 
-def parts(request):
-    allparts = Parts.objects.all()
-    cats = Category.objects.all()
-    data = {
-        "title": "Каталог запчастин Renault",
-        "description": "Переглянути каталог всі запчатини description",
-        "content": "content",
-        "allparts": allparts,
-        "cats": cats,
-        'cat_selected': 0,
-    }
-    return render(request, "main/parts.html", context=data)
+# def parts(request):
+#     allparts = Parts.objects.all()
+#     cats = Category.objects.all()
+#     data = {
+#         "title": "Каталог запчастин Renault",
+#         "description": "Переглянути каталог всі запчатини description",
+#         "content": "content",
+#         "allparts": allparts,
+#         "cats": cats,
+#         'cat_selected': 0,
+#     }
+#     return render(request, "main/parts.html", context=data)
 
-def show_category(request, category_id):
-    allparts = Parts.objects.filter(category_id=category_id).filter(auto_id=1)
-    cats = Category.objects.all()
+class SearchResult(ListView):
+    model = Parts
+    template_name = 'main/part.html'
 
-    data = {
-        "title": "Каталог запчастин Category",
-        "description": "Переглянути каталог category",
-        "content": "content",
-        "allparts": allparts,
-        "cats": cats,
-        'cat_selected': category_id,
-    }
-    return render(request, "main/parts.html", context=data)
+    def get_queryset(self, category_id):
+        category = Parts.objects.filter(category_id=category_id)
+        data = {
+            "title": "Каталог запчастин Renault",
+            "cat_selected": category_id,
+            "category": category,
+        }
+        return Parts.objects.filter(
+            Q(category_id=category_id)
+        )
 
-def postuser(request):
-    # получаем из данных запроса POST отправленные через форму данные
-    name = request.POST.get("name", "Undefined")
-    age = request.POST.get("age", 1)
-    lang = request.POST.get("languages", "AnyOne")
-    skil = request.POST.get("skils", "Nothing")
-    if len(name) == 0:
-        name = "NdsvdvdAE"
-    if request.method == "POST":
-        return HttpResponse(f""
-                        f"<div>Name: {name}  Age: {age}<div>"
-                        f"<div>Languages: {lang}</div>"
-                        f"<div>Skil:{skil}</div>")
-    return HttpResponse("<p>пустий запрос</p>")
+
+
+
+
+# def show_category(request, category_id):
+#     allparts = Parts.objects.filter(category_id=category_id)
+#     cats = Category.objects.all()
+#
+#     data = {
+#         "title": "Каталог запчастин Category",
+#         "description": "Переглянути каталог category",
+#         "content": "content",
+#         "allparts": allparts,
+#         "cats": cats,
+#         'cat_selected': category_id,
+#     }
+#     return render(request, "main/parts.html", context=data)
+#
+# def postuser(request):
+#     # получаем из данных запроса POST отправленные через форму данные
+#     name = request.POST.get("name", "Undefined")
+#     age = request.POST.get("age", 1)
+#     lang = request.POST.get("languages", "AnyOne")
+#     skil = request.POST.get("skils", "Nothing")
+#     if len(name) == 0:
+#         name = "NdsvdvdAE"
+#     if request.method == "POST":
+#         return HttpResponse(f""
+#                         f"<div>Name: {name}  Age: {age}<div>"
+#                         f"<div>Languages: {lang}</div>"
+#                         f"<div>Skil:{skil}</div>")
+#     return HttpResponse("<p>пустий запрос</p>")
 
 
 
